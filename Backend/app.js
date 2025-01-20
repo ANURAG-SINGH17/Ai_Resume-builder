58f07ab8eae04ec4bc4eadecc5ee5d3ba6331919
Anurag Singh <anuragaswal27@gmail.com>
2025-01-20 17:48:41 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,9 @@ const path = require('path');
 const usersRouter = require('./routes/user.routes')
 const aiRouter = require('./routes/ai.routes')
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://ai-resume-builder-eqgf02580-anurag-singh-coders-projects.vercel.app/',
+  }));
 app.use(express.json());
 app.use(cookiesParser());
 app.use(express.urlencoded({extended:true}));
@@ -19,10 +21,10 @@ const _dirname = path.resolve();
 app.use('/users', usersRouter)
 app.use('/ai', aiRouter)
 
-app.use(express.static(path.join(_dirname , "/frontend/dist")))
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname , 'frontend' , 'dist' , 'index.html'));
-})
+// app.use(express.static(path.join(_dirname , "/frontend/dist")))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(_dirname , 'frontend' , 'dist' , 'index.html'));
+// })
 
 app.get('/' , (req , res) => {
     res.send('hello server is start you are now in  / route')    
